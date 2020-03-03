ec3cf096f654f238155706e9132b8c7bf249c0ed
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-03-03 14:27:31 +0300

    Singleton task.

---
+++ Singleton/Singleton/DataBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+
+namespace Singleton
+{
+    public class DataBase
+    {
+        private static DataBase Instance;
+        private readonly string ConnectionString = @"Data Source=.\SQL EXPRESS;Initial Catalog=users db;Integrated Security=True";
+        private readonly SqlConnection Connection;
+ 
+        private DataBase()
+        {
+            Connection = new SqlConnection(ConnectionString);
+            try
+            {
+                Connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+        }
+
+        public static DataBase GetInstance()
+        {
+            return Instance ?? (DataBase.Instance = new DataBase());
+        }
+
+        public void CreateCommand(string queryString)
+        {
+            if (Instance != null)
+            {
+                SqlCommand command = new SqlCommand(queryString, Connection);
+                command.ExecuteNonQuery();
+            }
+            else
+            {
+                throw new System.Exception("You are not connected to a database.");
+            }
+        }
+    }
+
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DataBase dataBase = DataBase.GetInstance();
+                dataBase.CreateCommand("INSERT INTO Users (Name, Age) VALUES ('Tom', 18)");
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.Write(ex);
+            }
+        }
+    }
+}
