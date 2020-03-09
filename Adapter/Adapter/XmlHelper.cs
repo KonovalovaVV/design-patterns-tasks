cae17c51127b634a6f97c5c7b6b13c6ebdafa392
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-03-09 15:26:21 +0300

    Remove comment and unused usings.

--- Adapter/Adapter/XmlHelper.cs
+++ Adapter/Adapter/XmlHelper.cs
@@ -1,7 +1,5 @@
 ﻿using System.IO;
-using System.Xml;
 using System.Xml.Serialization;
-using Adapter.Book;
 
 namespace Adapter
 {
@@ -11,14 +9,11 @@ namespace Adapter
         {
             return DeserializeFromXmlString<T>(File.ReadAllText(filename));
         }
+
         public static T DeserializeFromXmlString<T>(string xml) where T : new()
         {
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
-            StringReader stringReader = new StringReader(xml);
-            var xmlObject = (T)xmlSerializer.Deserialize(stringReader);
-            return xmlObject;
+            return (T)xmlSerializer.Deserialize(new StringReader(xml));
         }
     }
-}
-//
-//<catalog xmlns = "urn:oasis:names:tc:entity:xmlns:xml:catalog" >
+}    
